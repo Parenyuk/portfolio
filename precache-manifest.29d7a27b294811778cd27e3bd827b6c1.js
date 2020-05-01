17a8313cca93f63c2b96894c9c9c6183ee188b19
Victor <ParenVic@i.ua>
2020-05-01 11:55:56 +0300

    Updates

---
+++ precache-manifest.29d7a27b294811778cd27e3bd827b6c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b297c66efe9ccd23b096fa21112d8e1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5740e2c214f4ee1dc6c8",
+    "url": "/portfolio/static/css/main.ca5dc48d.chunk.css"
+  },
+  {
+    "revision": "fc90cd49192a06d8c356",
+    "url": "/portfolio/static/js/2.93915c27.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.93915c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5740e2c214f4ee1dc6c8",
+    "url": "/portfolio/static/js/main.00418d68.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
