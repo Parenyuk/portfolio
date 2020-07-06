2330dfb27dc547f49498db084a6d035230c8c1e4
Victor <ParenVic@i.ua>
2020-07-06 16:11:20 +0300

    Updates

---
+++ precache-manifest.65c13471138e35f9484657c50980182a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96a01441c15b8a4d554d96aa607fcc9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c14900767bce871d3917",
+    "url": "/portfolio/static/css/main.37fc1a90.chunk.css"
+  },
+  {
+    "revision": "5d7f8037895cbd840cf7",
+    "url": "/portfolio/static/js/2.b314ffca.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.b314ffca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14900767bce871d3917",
+    "url": "/portfolio/static/js/main.bba718cc.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "030190a1b07268262e35aaa3ddfb4b24",
+    "url": "/portfolio/static/media/calculator.030190a1.jpg"
+  },
+  {
+    "revision": "c14da210ece1241c23521e65ebb6dad9",
+    "url": "/portfolio/static/media/facebook.c14da210.svg"
+  },
+  {
+    "revision": "fc23b90236f87b11147810f35f4c5293",
+    "url": "/portfolio/static/media/instagram.fc23b902.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "3983bbd9fad4ea7591df057985dbd5f4",
+    "url": "/portfolio/static/media/socialNetwork.3983bbd9.jpg"
+  },
+  {
+    "revision": "7b039a73beb86926c970f12ecece47b2",
+    "url": "/portfolio/static/media/telegram.7b039a73.svg"
+  },
+  {
+    "revision": "84e20b50b5b8afaaa0c6ac4b4a582ae6",
+    "url": "/portfolio/static/media/todo.84e20b50.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5281b9f596df7538411e059443808566.js"
+  "/portfolio/precache-manifest.65c13471138e35f9484657c50980182a.js"
 );
 
 self.addEventListener('message', (event) => {
